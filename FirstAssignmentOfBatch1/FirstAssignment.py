a7fdb23deb6ad615272e7f1fcbb3907d7f8f0c60
School_of Mindlight <schoolofmindlight2018@gmail.com>
2022-07-05 09:20:08 +0600

    this file is submited by, Akash bhai

---
+++ FirstAssignmentOfBatch1/FirstAssignment.py
@@ -0,0 +1,9 @@
+# written by Mohammad akash Sarkar
+mytext = """
+Twinkle, twinkle, little star,\t
+How I wonder what you are!\t
+Up above the world so high,\t
+Like a diamond in the sky.
+
+"""
+print(mytext)
