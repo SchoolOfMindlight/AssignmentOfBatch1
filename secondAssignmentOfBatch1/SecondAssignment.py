e90f4360153dbe3df7078348cd7620667a9025e0
School_of Mindlight <schoolofmindlight2018@gmail.com>
2022-08-21 12:38:55 +0600

    this is the second assignment folder

---
+++ secondAssignmentOfBatch1/SecondAssignment.py
@@ -0,0 +1,5 @@
+Num1 = input("Enter your first number:")
+Num2 = input("Enter your second number:")
+sum = Num1 + Num2
+sum = float (sum)
+print("Your result is:",sum)
---
+++ secondAssignmentOfBatch1/second-assignment.py
@@ -0,0 +1,5 @@
+# Mywork
+Num1 = input("Enter your first registration number:")
+Num2 = input("Enter your second registration number:")
+sum = float(Num1) + float(Num2)
+print("Your result is:",sum)
