2500e8e48dadf22e016cbfb3247af7cc3615bea8
School_of Mindlight <schoolofmindlight2018@gmail.com>
2022-08-21 12:41:40 +0600

    This assignment isn't complete

---
+++ fourthAssignmentOfBatch1/Mycalculetter.py
@@ -0,0 +1,14 @@
+#My task about calculetter
+# akash task.
+
+# num1 = float(input("Enterenter number : "))
+num1 = float(input("Enter a number : "))
+operator = input('Enter a operator:')
+# num2 = float(input("Enter enother imput number: "))
+num2 = float(input("Enter enother number: "))
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print( num1 *num2)
+elif operator == '/':
+    print(num1 / num2)
